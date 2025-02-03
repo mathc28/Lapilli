ccf2c6af7902ef2c887550fcab126068fad84c04
mathc28 <cocumathieu@gmail.com>
2025-02-03 17:12:47 +0100

    ajout du projet sur github

---
+++ main.js
@@ -0,0 +1,11 @@
+function openModal(img) {
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImg");
+  
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  }
+  
+  function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+  }
